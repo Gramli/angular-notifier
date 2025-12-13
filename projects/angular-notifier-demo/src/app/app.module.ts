6026ad03bcac9deb1e4dbb9da18061dd88aff2a4
Gramli <Gramli@seznam.cz>
2025-12-13 21:50:35 +0100

    - fix demo app

--- projects/angular-notifier-demo/src/app/app.module.ts
+++ projects/angular-notifier-demo/src/app/app.module.ts
@@ -19,7 +19,7 @@ const customNotifierOptions: NotifierOptions = {
       gap: 10,
     },
   },
-  theme: 'bootstrap',
+  theme: 'material',
   behaviour: {
     autoHide: false,
     onClick: false,
